50421ab7a12aab0481db3f8a37a3827a615149be
Ashok PS <ashok.somasundaram20@gmail.com>
2019-03-06 10:26:51 +0530

    class file Implementation changes

--- Automation/PageObjects/LoginPageClass.cs
+++ Automation/PageObjects/LoginPageClass.cs
@@ -1,12 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
 
-namespace Automation.PageObjects
+namespace LoginPageObjects
 {
-    class LoginPageClass
+    [TestClass]
+    public class LoginPage
     {
+        private IWebDriver driver;
+        [FindsBy(How = How.XPath, Using = "//input[@id='txtUsername']")]
+        [CacheLookup]
+        public IWebElement UserName { get; set; }
+        [FindsBy(How = How.XPath, Using = "//input[@id='txtPassword']")]
+        [CacheLookup]
+        public IWebElement PassWord { get; set; }
+        [FindsBy(How = How.XPath, Using = "//input[@id='Button1']")]
+        [CacheLookup]
+        public IWebElement LoginButton { get; set; }
+        [FindsBy(How = How.XPath, Using = "//div[@class='popover-content']")]
+        [CacheLookup]
+        public IWebElement PopOverDialog;
+        [FindsBy(How = How.XPath, Using = "//div[@class='popover-content']//div//button[@id='btnYesLogout']")]
+        [CacheLookup]
+        public IWebElement PopOverYesLogout;
+        [FindsBy(How = How.XPath, Using = "//div[@class='popover-content']//div//button[@id='btnNoLogout']")]
+        [CacheLookup]
+        public IWebElement PopOverNoLogout;
+        [FindsBy(How = How.XPath, Using = "//span[@id='cart-count']")]
+        [CacheLookup]
+        public IWebElement CartCount;
+        [FindsBy(How = How.LinkText, Using = "Logout")]
+        [CacheLookup]
+        public IWebElement LogoutOption;
+        [FindsBy(How = How.XPath, Using = "rj ( Logged in as rjurbansc )")]
+        [CacheLookup]
+        public IWebElement LoggedUser;
+        [FindsBy(How = How.XPath, Using = "//div[@id='profileModal']")]
+        [CacheLookup]
+        public IWebElement ProfileModal;
+        [FindsBy(How = How.XPath, Using = "//div[@id='profileModal']//button[@class='btn'][contains(text(),'Cancel')]")]
+        [CacheLookup]
+        public IWebElement ProfileModalCancelOption;
+
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+        public void LoginToApp()
+        {
+            UserName.SendKeys("yuva_hm_qa");
+            PassWord.SendKeys("welcome1");
+            LoginButton.Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            if (ProfileModal.Displayed)
+            {
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+                ProfileModalCancelOption.Click();
+            }
+        }
     }
 }
--- Automation/PageObjects/ShoppingCartPageClass.cs
+++ Automation/PageObjects/ShoppingCartPageClass.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
 
-namespace Automation.PageObjects
+namespace ShoppingCartPageObjects
 {
-    class ShoppingCartPageClass
+    [TestClass]
+    public class ShoppingCartPage
     {
+        public IWebDriver driver;
+        [FindsBy(How = How.XPath, Using = "/html[1]/body[1]/div[1]/div[1]/div[1]/div[3]/div[1]/button[1]")]
+        [CacheLookup]
+        public IWebElement ContinueOption;
+        [FindsBy(How = How.XPath, Using = "//div[@id='shopCart1']//div[3]//div[2]//button[1]")]
+        [CacheLookup]
+        public IWebElement MultipleDeleteShoppingCartOption;
+        [FindsBy(How = How.XPath, Using = "//div[@id='shopCart1']//div[3]//div[2]//button[2]")]
+        [CacheLookup]
+        public IWebElement MultipleClearShoppingCartoption;
+        [FindsBy(How = How.XPath, Using = "//img[@src='https://qachecknet.checkpt.com/Images/icon_trash.png']")]
+        [CacheLookup]
+        public IWebElement TrashIcon;
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
     }
 }
