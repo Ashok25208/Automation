60dd09f189cd1f8c111ee6911fc0ec87d4a8a381
Ashok PS <ashok.somasundaram20@gmail.com>
2019-03-08 14:13:27 +0530

    SMTO changes

--- Automation/Utils/MailReport.cs
+++ Automation/Utils/MailReport.cs
@@ -13,34 +13,26 @@ namespace Automation.Utils
         public void Send_Report_In_Mail()
         {
             MailMessage mail = new MailMessage();
-            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
-            mail.From = new MailAddress("sender_mail_address");
-            mail.To.Add("mail_lists_of_recipients");
-
+            SmtpClient SmtpServer = new SmtpClient("smtp.office365.com");
+            mail.From = new MailAddress("ashok.somasundaram@checkpt.com");
+            mail.To.Add("Arun.Soundrapandian@checkpt.com");
             StringBuilder TimeAndDate = new StringBuilder(DateTime.Now.ToString());
             TimeAndDate.Replace("/", "_");
             TimeAndDate.Replace(":", "_");
-
             mail.Subject = "Automation Test Report_"+TimeAndDate;
-
             mail.Body = "Please find the attached report to get details.";
-
             string actualPath = AppDomain.CurrentDomain.BaseDirectory.Replace("\\bin\\Debug", "\\Test_Execution_Report"); //Reports should store in Test_Execution_Reports folder
-
             var mostRecentlyModified = Directory.GetFiles(actualPath, "*.html")
             .Select(f => new FileInfo(f))
             .OrderByDescending(fi => fi.LastWriteTime)
             .First()
             .FullName;
-
             Attachment attachment;
             attachment = new Attachment(mostRecentlyModified);
             mail.Attachments.Add(attachment);
-
             SmtpServer.Port = 587;
-            SmtpServer.Credentials = new System.Net.NetworkCredential("sender_mail_address", "sender_mail_account_password");
+            SmtpServer.Credentials = new System.Net.NetworkCredential("ashok.somasundaram@checkpt.com", "ash@$1227");
             SmtpServer.EnableSsl = true;
-
             SmtpServer.Send(mail);
 
         }
